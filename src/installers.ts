e34bde8a46164c6b88d537ada32564619e670831
Ellie Peterson <ekp2h92@gmail.com>
2022-03-09 22:49:44 -0500

    🎨 Small changes

--- src/installers.ts
+++ src/installers.ts
@@ -157,22 +157,6 @@ export const notInstallableMod: VortexWrappedInstallFunc = (
 ) => {
   throw new Error("Should never get here");
 };
-// /**
-//  * Installs files as is
-//  * @param files a list of files to be installed
-//  * @returns a promise with an array detailing what files to install and how
-//  */
-// function installWithCorrectStructure(files: string[]) {
-//   // Everything is placed in the correct structure, so just install it as is
-//   // log('info', 'installing files for a game')
-//   const instructions = files.map((file: string) => {
-//     log("debug", "Installing file found on standard path: ", file);
-//     return {
-//       type: "copy",
-//       source: file,
-//       destination: path.join(file),
-//     };
-//   });
 
 //   return Promise.resolve({ instructions });
 // }
@@ -264,7 +248,7 @@ const allCanonicalArchiveOnlyFiles = (files: string[]) =>
 // CET mods are detected by:
 //
 // Canonical:
-//  - bin\x64\plugins\cyber_engine_tweaks\mods\MODNAME\init.lua
+//  - .\bin\x64\plugins\cyber_engine_tweaks\mods\MODNAME\init.lua
 //  - .\r6\scripts\[modname]\*.reds
 //
 // Fixable: no
@@ -289,16 +273,16 @@ export const testForCetMod: VortexWrappedTestSupportedFunc = (
     return Promise.resolve({ supported: false, requiredFiles: [] });
   }
 
-  const hasIniFilesInANamedModDir = moddir.children.some(
+  const hasCetFilesInANamedModDir = moddir.children.some(
     (child) => child.getChild(CET_MOD_CANONICAL_INIT_FILE) !== null,
   );
 
-  if (hasIniFilesInANamedModDir) {
-    log("info", `Matching CET installer: ${hasIniFilesInANamedModDir}`);
+  if (hasCetFilesInANamedModDir) {
+    log("info", `Matching CET installer: ${hasCetFilesInANamedModDir}`);
   }
 
   return Promise.resolve({
-    supported: hasIniFilesInANamedModDir,
+    supported: hasCetFilesInANamedModDir,
     requiredFiles: [],
   });
 };
@@ -348,36 +332,6 @@ const matchRedscript = (file: string) =>
 const allRedscriptFiles = (files: string[]): string[] =>
   files.filter(matchRedscript);
 
-/**
- * A helper for redscript files
- * @param redFiles redscript files
- * @param genericModName a mod folder if everyhting is awful
- * @returns an array of instructions for the files
- */
-/*
-function redScriptInstallationHelper(
-  redFiles: string[],
-  _genericModName: string,
-) {
-  //   let files = redFiles.filter((f) => !path.extname(f));
-
-  // Ensure all the RedScript files are in their own mod directory. (Should have been checked beforehand)
-  const normalizedFiltered = redFiles.map((file: string) =>
-    file.includes(REDS_MOD_CANONICAL_PATH_PREFIX)
-      ? file
-      : path.join(REDS_MOD_CANONICAL_PATH_PREFIX, file),
-  );
-
-  // Set destination to be 'r6/scripts/ModFolder/[file].reds'
-  const instructions = normalizedFiltered.map((file: string) => ({
-    type: "copy",
-    source: file,
-    destination: path.join(REDS_MOD_CANONICAL_PATH_PREFIX, path.basename(file)),
-  }));
-
-  return instructions;
-} */
-
 export const testForRedscriptMod: VortexWrappedTestSupportedFunc = (
   api: VortexAPI,
   log: VortexLogFunc,
@@ -389,7 +343,7 @@ export const testForRedscriptMod: VortexWrappedTestSupportedFunc = (
   const redscriptFiles = allRedscriptFiles(files);
 
   // eslint-disable-next-line no-console
-  console.log({ redscriptFiles });
+  log("debug", "redscriptFiles: ", { redscriptFiles });
   // We could do more detection here but the
   // installer will already need to duplicate
   // all that. Maybe just check whether there
@@ -690,92 +644,6 @@ export const testAnyOtherModFallback: VortexWrappedTestSupportedFunc = (
   });
 };
 
-// /**
-//  * A check with complex logic that I wanted pulled out of the main function
-//  * @param cleanArchive Whether the archive files are correct or not
-//  * @param cleanReds whether the redscript files are correct or not
-//  * @param cleanCet Whether the cet files are correct or not
-//  * @returns true when all files that need to be in the right place are in the right place, false otherwise
-//  */
-// function complexCleanCheckLogic(cleanArchive, cleanReds, cleanCet) {
-//   // At least one of the files needs to be defined, otherwise it can't be garunteed they are all okay or something.
-//   if (
-//     ((cleanArchive !== undefined && cleanArchive === true) ||
-//       cleanArchive === undefined) &&
-//     ((cleanReds !== undefined && cleanReds === true) ||
-//       cleanReds === undefined) &&
-//     ((cleanCet !== undefined && cleanCet === true) || cleanCet === undefined) &&
-//     (cleanArchive !== undefined ||
-//       cleanReds !== undefined ||
-//       cleanCet !== undefined)
-//   ) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-
-// /**
-//  * A helper for any other files
-//  * @param files any files
-//  * @param genericModName a mod folder if everyhting is awful
-//  * @returns an array of instructions for the files
-//  */
-// function genericFileInstallationHelper(files: string[], genericModName: string) {
-//   files = files.filter((f) => !path.extname(f));
-//   // if the leftover file is under the archive path, we can ignore it as it is not an archive file and the game wouldn't load it
-//   let filtered = files.filter((file: string) => {
-//     !file.includes(ARCHIVE_MOD_PATH);
-//   });
-
-//   // else if it is a part of the redscript or cet path, we should install it in same relative location as we do for those scripts
-//   let cetFiles = files.filter((file: string) => {
-//     file.includes(CET_SCRIPT_PATH);
-//   });
-//   let normalizedCET = cetFiles.map((file: string) => {
-//     let maybeModInFolder = file.replace(CET_SCRIPT_PATH, "");
-//     let parted = path.dirname(maybeModInFolder).split(path.sep);
-//     if (parted.length > 1) {
-//       return maybeModInFolder;
-//     } else {
-//       return path.join(CET_SCRIPT_PATH, genericModName, maybeModInFolder);
-//     }
-//   });
-//   let redFiles = files.filter((file: string) => {
-//     file.includes(REDSCRIPT_PATH);
-//   });
-//   let normalizedReds = redFiles.map((file: string) => {
-//     let maybeModInFolder = file.replace(REDSCRIPT_PATH, "");
-//     let parted = path.dirname(maybeModInFolder).split(path.sep);
-//     if (parted.length > 1) {
-//       return maybeModInFolder;
-//     } else {
-//       return path.join(REDSCRIPT_PATH, genericModName, maybeModInFolder);
-//     }
-//   });
-
-//   // otherwise, install it in same location that it is in in the archive, as it is likely already in the correct location
-//   let leftOvers = filtered.filter((file: string) => {
-//     !cetFiles.includes(file) && !redFiles.includes(file);
-//   });
-
-//   let rebuiltFiles = normalizedCET.concat(normalizedReds, leftOvers);
-
-//   let instructions = rebuiltFiles.map((file: string) => {
-//     log(
-//       "debug",
-//       "Correcting Generic file found with a bad path: ".concat(file)
-//     );
-//     return {
-//       type: "copy",
-//       source: file,
-//       destination: path.join(file),
-//     };
-//   });
-
-//   return instructions;
-// }
-
 /**
  * Installs files while correcting the directory structure as we go.
  * @param files a list of files to be installed
@@ -844,7 +712,7 @@ const addPriorityFrom = (start: number) => {
 //
 // Using Vortex parameter names here for convenience.
 //
-export const installerPipeline: InstallerWithPriority[] = [
+const installers: Installer[] = [
   {
     type: InstallerType.CET,
     id: "cp2077-cet-mod",
@@ -937,4 +805,9 @@ export const installerPipeline: InstallerWithPriority[] = [
     testSupported: testAnyOtherModFallback,
     install: installAnyModWithBasicFixes,
   },
-].reduce(addPriorityFrom(PRIORITY_STARTING_NUMBER), []);
+];
+
+export const installerPipeline: InstallerWithPriority[] = installers.reduce(
+  addPriorityFrom(PRIORITY_STARTING_NUMBER),
+  [],
+);
