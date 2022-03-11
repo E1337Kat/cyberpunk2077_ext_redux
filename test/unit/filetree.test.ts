00519a7a1ddcbb05f3ed4205975c421e892415d0
Auska <auska+code@auska.works>
2022-03-11 22:00:07 +0200

    More FileTree tests

--- test/unit/filetree.test.ts
+++ test/unit/filetree.test.ts
@@ -1,28 +1,37 @@
 import path from "path";
-import { fileTreeFromPaths, findAllInTree } from "../../src/filetree";
+import {
+  fileTreeFromPaths,
+  findAllSubdirsWithSome,
+  findTopmostSubdirsWithSome,
+  findAllFiles,
+  FILETREE_ROOT,
+  FILETREE_TOPLEVEL,
+} from "../../src/filetree";
+
+const paths = [
+  path.normalize("topf1.seek"),
+  path.normalize("topf2.seek"),
+  path.normalize("topf3.notseek"),
+  path.normalize("sub1/"),
+  path.normalize("sub1/sub12/"),
+  path.normalize("sub1/sub12/f12.seek"),
+  path.normalize("sub1/sub12/f12.notseek"),
+  path.normalize("sub2/"),
+  path.normalize("sub2/sub22/f22.seek"),
+  path.normalize("sub2/sub23/f23.notseek"),
+  path.normalize("sub2/sub24/"),
+  path.normalize("sub2/f24.seek"),
+  path.normalize("sub2/f2r.notseek"),
+  path.normalize("sub2/x2h"),
+];
+
+const matchSeek = (f) => path.extname(f) === ".seek";
 
 describe("FileTree", () => {
-  test("find actually works", () => {
-    const paths = [
-      path.normalize("topf1.seek"),
-      path.normalize("topf2.seek"),
-      path.normalize("topf3.notseek"),
-      path.normalize("sub1/"),
-      path.normalize("sub1/sub12/"),
-      path.normalize("sub1/sub12/f12.seek"),
-      path.normalize("sub1/sub12/f12.notseek"),
-      path.normalize("sub2/"),
-      path.normalize("sub2/sub22/f22.seek"),
-      path.normalize("sub2/sub23/f23.notseek"),
-      path.normalize("sub2/sub24/"),
-      path.normalize("sub2/f24.seek"),
-      path.normalize("sub2/f2r.notseek"),
-      path.normalize("sub2/x2h"),
-    ];
-
+  test("findAllFiles", () => {
     const fileTree = fileTreeFromPaths(paths);
 
-    const found = findAllInTree(fileTree, (f) => path.extname(f) === ".seek");
+    const found = findAllFiles(matchSeek, fileTree);
 
     expect(found).toEqual([
       path.normalize("topf1.seek"),
@@ -30,6 +39,66 @@ describe("FileTree", () => {
       path.normalize("sub1/sub12/f12.seek"),
       path.normalize("sub2/sub22/f22.seek"),
       path.normalize("sub2/f24.seek"),
+    ]);
+  });
+
+  test("findAllSubdirsWithSome excluding toplevel", () => {
+    const fileTree = fileTreeFromPaths(paths);
+
+    const found = findAllSubdirsWithSome(
+      FILETREE_TOPLEVEL,
+      matchSeek,
+      fileTree,
+    );
+
+    expect(found).toEqual([
+      path.normalize("sub1/sub12"),
+      path.normalize("sub2/sub22"),
+      path.normalize("sub2"),
+    ]);
+  });
+
+  test("findAllSubDirsWithSome including toplevel", () => {
+    const fileTree = fileTreeFromPaths(paths);
+
+    const found = findAllSubdirsWithSome(FILETREE_ROOT, matchSeek, fileTree);
+
+    expect(found).toEqual([
+      path.normalize("."),
+      path.normalize("sub1/sub12"),
+      path.normalize("sub2/sub22"),
+      path.normalize("sub2"),
+    ]);
+  });
+
+  test("findTopmostSubdirsWithSome excluding toplevel", () => {
+    const fileTree = fileTreeFromPaths(paths);
+
+    const found = findTopmostSubdirsWithSome(
+      FILETREE_TOPLEVEL,
+      matchSeek,
+      fileTree,
+    );
+
+    expect(found).toEqual([
+      path.normalize("sub1/sub12"),
+      path.normalize("sub2"),
+    ]);
+  });
+
+  test("findTopmostSubdirsWithSome including toplevel", () => {
+    const fileTree = fileTreeFromPaths(paths);
+
+    const found = findTopmostSubdirsWithSome(
+      FILETREE_ROOT,
+      matchSeek,
+      fileTree,
+    );
+
+    expect(found).toEqual([
+      path.normalize("."),
+      path.normalize("sub1/sub12"),
+      path.normalize("sub2"),
     ]);
   });
 });
