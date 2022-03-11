d287a66401fb05e4e90fdc9a5db25041c1be6671
Auska <auska+code@auska.works>
2022-03-11 22:00:40 +0200

    FileTree implementation (both way too much/little)

--- src/filetree.ts
+++ src/filetree.ts
@@ -1,48 +1,93 @@
+/* eslint-disable no-underscore-dangle */
 import nodejsPath from "path";
 import KeyTree from "key-tree";
 
 /*
 export type FileTree = {
   sep: Path;
-  '$': FileTreeNode;
+  '$': Filenode;
   // Methods
   add: (key: Path, values: Path[]) => void;
   get: (key: Path) => Path[];
   getSub: (key: Path, grouped: boolean) => Path[] | unknown; // grouped nodejsPath object actually
   // Private
-  _getNode: (key: Path) => FileTreeNode;
+  _getNode: (key: Path) => Filenode;
 }
 */
 
 export type FileTree = KeyTree;
-export type Path = string;
-export type PathFilter = (path: Path) => boolean;
+export type PathFilter = (path: string) => boolean;
+
+// -.-
+export type MaybeFileTree = FileTree | undefined;
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface PathArray extends Array<PathArray | Path> {}
+export interface PathArray extends Array<PathArray | string> {}
 
-export const FILETREE_ROOT = nodejsPath.dirname(
+export const FILETREE_ROOT = "";
+
+export const FILETREE_TOPLEVEL = nodejsPath.dirname(
   "This has no directory so it normalizes to . or current dir basically",
 );
 
-export const fileTreeFromPaths = (paths: Path[]): FileTree => {
+// As with nodejsPath.dirname(), leave out the trailing separator
+const dirName = (node): string => nodejsPath.join(...node.fullPath);
+
+// The tree stores '.' at the same level as the top-level subdirs,
+// which both makes sense and absolutely does not.
+const actualChildren = (node) => {
+  if (!node) {
+    return [];
+  }
+
+  switch (node.key) {
+    case FILETREE_TOPLEVEL:
+      return node.parent.children.filter((c) => c.key !== FILETREE_TOPLEVEL);
+    case FILETREE_ROOT:
+      return node.children;
+    default:
+      return node.children;
+  }
+};
+
+const findFilesRecursive = (predicate: PathFilter, node): PathArray => {
+  const subMatches: PathArray = node.children.flatMap((c) =>
+    findFilesRecursive(predicate, c),
+  );
+
+  const matches = node.values.filter(predicate);
+
+  return subMatches.concat(matches);
+};
+
+const findDirsRecursive = (
+  breakEarly: boolean,
+  predicate: PathFilter,
+  node,
+): PathArray => {
+  const selfMaybe = node.values.some(predicate) ? [dirName(node)] : [];
+
+  if (selfMaybe.length > 0 && breakEarly) {
+    return selfMaybe;
+  }
+
+  const subMatches: PathArray = node.children.flatMap((c) =>
+    findDirsRecursive(breakEarly, predicate, c),
+  );
+
+  return subMatches.concat(selfMaybe);
+};
+
+// Interface
+
+export const fileTreeFromPaths = (paths: string[]): FileTree => {
   const tree = new KeyTree({ separator: nodejsPath.sep });
   paths.forEach((path) => tree.add(nodejsPath.dirname(path), path));
 
   return tree;
 };
 
-export const filesInDir = (
-  tree: FileTree,
-  dir: Path,
-  fileFilter?: PathFilter,
-): Path[] => {
-  const files = tree.get(dir);
-
-  return fileFilter !== undefined ? files.filter(fileFilter) : files;
-};
-
-export const subdirPaths = (tree: FileTree, dir: Path): Path[] => {
+export const subdirPaths = (dir: string, tree: FileTree): string[] => {
   const node = tree._getNode(dir); // eslint-disable-line no-underscore-dangle
 
   if (node === undefined || node.children.length < 1) {
@@ -52,17 +97,42 @@ export const subdirPaths = (tree: FileTree, dir: Path): Path[] => {
   return node.children.map((subdir) => nodejsPath.join(dir, subdir.key));
 };
 
-const findRecursive = (treeNode, predicate: PathFilter): PathArray => {
-  const subMatches: PathArray = treeNode.children.flatMap((c) =>
-    findRecursive(c, predicate),
+export const filesIn = (
+  dir: string,
+  tree: FileTree,
+  predicate?: PathFilter,
+): PathArray => (predicate ? tree.get(dir).filter(predicate) : tree.get(dir));
+
+export const allFilesInDirAndSubdirs = (
+  dir: string,
+  tree: FileTree,
+): PathArray => tree.getSub(dir);
+
+export const dirWithSome = (
+  dir: string,
+  predicate: PathFilter,
+  tree: FileTree,
+): boolean => tree.get(dir).some(predicate);
+
+export const findAllFiles = (
+  predicate: PathFilter,
+  tree: FileTree,
+): PathArray => findFilesRecursive(predicate, tree.$);
+
+export const findAllSubdirsWithSome = (
+  dir: string,
+  predicate: PathFilter,
+  tree: FileTree,
+): PathArray =>
+  actualChildren(tree._getNode(dir)).flatMap((sub) =>
+    findDirsRecursive(false, predicate, sub),
   );
 
-  const matches = treeNode.values.filter(predicate);
-
-  return subMatches.concat(matches);
-};
-
-export const findAllInTree = (
+export const findTopmostSubdirsWithSome = (
+  dir: string,
+  predicate: PathFilter,
   tree: FileTree,
-  predicate: (path: Path) => boolean,
-): PathArray => findRecursive(tree.$, predicate);
+): PathArray =>
+  actualChildren(tree._getNode(dir)).flatMap((sub) =>
+    findDirsRecursive(true, predicate, sub),
+  );
