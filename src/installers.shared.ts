c3a37cf0d7bf0359425c9c1548e52d8a79ea6c38
Ellie P <E1337Kat@hotmail.com>
2022-03-27 17:22:38 -0400

    Revert "Use A Namespaced Synthetic Modname"

--- src/installers.shared.ts
+++ src/installers.shared.ts
@@ -1,6 +1,5 @@
 import path from "path";
 import { FileTree, FILETREE_ROOT } from "./filetree";
-import { EXTENSION_NAME_INTERNAL } from "./index.metadata";
 import {
   LayoutToInstructions,
   NoInstructions,
@@ -9,12 +8,6 @@ import {
 } from "./installers.layouts";
 
 import { VortexApi, VortexInstruction } from "./vortex-wrapper";
-
-// Vortex gives us a 'destination path', which is actually
-// the tempdir in which the archive is expanded into for
-// the duration of the installation.
-export const makeSyntheticName = (vortexDestinationPath: string) =>
-  `${EXTENSION_NAME_INTERNAL}-${path.basename(vortexDestinationPath, ".installing")}`;
 
 // Source to dest path mapping helpers
 export const toSamePath = (f: string) => [f, f];
@@ -84,6 +77,7 @@ export const useAllMatchingLayouts = (
     .map((layout) => layout(api, modName, fileTree))
     .filter((instructions) => instructions !== NoInstructions.NoMatch);
 
+  console.log({ allInstructions });
   const someValidInstructions: Instructions[] = allInstructions.filter(
     (maybe): maybe is Instructions => (maybe as Instructions).kind !== undefined,
   );
