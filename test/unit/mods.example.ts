19fd432515b3d7eecc1cf223bbeb338ae84566d5
Ellie Peterson <ekp2h92@gmail.com>
2022-04-08 17:30:15 -0400

    ✅ Fuck it, why test an edge case's edge case. Maybe don't package mods stupidly

--- test/unit/mods.example.ts
+++ test/unit/mods.example.ts
@@ -870,30 +870,6 @@ export const RedscriptModShouldPromptForInstall = new Map<
   ExamplePromptInstallableMod
 >(
   Object.entries({
-    redsWithRedsInToplevelAndCannonPromptsOnConflictForFallback: {
-      expectedInstallerType: InstallerType.Redscript,
-      inFiles: [
-        path.join(`script.reds`),
-        ...REDS_PREFIXES,
-        path.join(`${REDS_PREFIX}/rexmod/`),
-        path.join(`${REDS_PREFIX}/rexmod/patch.reds`),
-      ],
-      proceedLabel: InstallChoices.Proceed,
-      proceedOutInstructions: [
-        {
-          type: "copy",
-          source: path.join(`script.reds`),
-          destination: path.join(`${REDS_PREFIX}/${FAKE_MOD_NAME}/script.reds`),
-        },
-        {
-          type: "copy",
-          source: path.join(`${REDS_PREFIX}/rexmod/patch.reds`),
-          destination: path.join(`${REDS_PREFIX}/${FAKE_MOD_NAME}/rexmod/patch.reds`),
-        },
-      ],
-      cancelLabel: InstallChoices.Cancel,
-      cancelErrorMessage: expectedUserCancelMessageFor(InstallerType.Redscript),
-    },
     redsWithRedsInToplevelSubdirPromptsOnConflictForFallback: {
       expectedInstallerType: InstallerType.Redscript,
       inFiles: [path.join(`rexmod/script.reds`)],
