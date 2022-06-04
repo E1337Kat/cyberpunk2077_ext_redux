5fb6c015001fa8d017bb89f920e2db90d7743994
Auska <13802421+effs@users.noreply.github.com>
2022-06-04 03:29:37 +0300

    ♻️ Extract Redscript and Core Reds test examples to their own files

---
+++ test/unit/mods.example.core.redscript.ts
@@ -0,0 +1,52 @@
+import path from "path";
+import { InstallerType } from "../../src/installers.types";
+import {
+  ExampleSucceedingMod,
+  ExamplesForType,
+  ExampleFailingMod,
+  ExamplePromptInstallableMod,
+} from "./utils.helper";
+
+const CoreRedscriptInstall = new Map<string, ExampleSucceedingMod>(
+  Object.entries({
+    coreRedscriptInstall: {
+      expectedInstallerType: InstallerType.CoreRedscript,
+      inFiles: [
+        path.join("engine/"),
+        path.join("engine/config/"),
+        path.join("engine/config/base/"),
+        path.join("engine/config/base/scripts.ini"),
+        path.join("engine/tools/"),
+        path.join("engine/tools/scc.exe"),
+        path.join("r6/"),
+        path.join("r6/scripts/"),
+        path.join("r6/scripts/redscript.toml"),
+      ].map(path.normalize),
+      outInstructions: [
+        {
+          type: "copy",
+          source: path.join("engine/config/base/scripts.ini"),
+          destination: path.join("engine/config/base/scripts.ini"),
+        },
+        {
+          type: "copy",
+          source: path.join("engine/tools/scc.exe"),
+          destination: path.join("engine/tools/scc.exe"),
+        },
+        {
+          type: "copy",
+          source: path.join("r6/scripts/redscript.toml"),
+          destination: path.join("r6/scripts/redscript.toml"),
+        },
+      ],
+    },
+  }),
+);
+
+const examples: ExamplesForType = {
+  AllExpectedSuccesses: CoreRedscriptInstall,
+  AllExpectedDirectFailures: new Map<string, ExampleFailingMod>(),
+  AllExpectedPromptInstalls: new Map<string, ExamplePromptInstallableMod>(),
+};
+
+export default examples;
---
+++ test/unit/mods.example.redscript.ts
@@ -0,0 +1,174 @@
+import path from "path";
+import { InstallerType } from "../../src/installers.types";
+import { InstallChoices } from "../../src/ui.dialogs";
+import {
+  ExampleSucceedingMod,
+  REDS_PREFIXES,
+  REDS_PREFIX,
+  FAKE_MOD_NAME,
+  ExamplePromptInstallableMod,
+  expectedUserCancelMessageFor,
+  ExamplesForType,
+  ExampleFailingMod,
+} from "./utils.helper";
+
+const RedscriptModShouldSucceed = new Map<string, ExampleSucceedingMod>(
+  Object.entries({
+    redsWithBasedirAndCanonicalFilesInstallsToSubdir: {
+      expectedInstallerType: InstallerType.Redscript,
+      inFiles: [
+        ...REDS_PREFIXES,
+        path.join(`${REDS_PREFIX}/yay.reds`),
+        path.join(`${REDS_PREFIX}/rexmod/`),
+        path.join(`${REDS_PREFIX}/rexmod/script.reds`),
+      ],
+      outInstructions: [
+        {
+          type: "copy",
+          source: path.join(`${REDS_PREFIX}\\yay.reds`),
+          destination: path.join(`${REDS_PREFIX}\\${FAKE_MOD_NAME}\\yay.reds`),
+        },
+        {
+          type: "copy",
+          source: path.join(`${REDS_PREFIX}\\rexmod\\script.reds`),
+          destination: path.join(`${REDS_PREFIX}\\${FAKE_MOD_NAME}\\rexmod\\script.reds`),
+        },
+      ],
+    },
+    redsWithSingleFileCanonical: {
+      expectedInstallerType: InstallerType.Redscript,
+      inFiles: [
+        ...REDS_PREFIXES,
+        path.join(`${REDS_PREFIX}/rexmod/`),
+        path.join(`${REDS_PREFIX}/rexmod/script.reds`),
+      ],
+      outInstructions: [
+        {
+          type: "copy",
+          source: path.join(`${REDS_PREFIX}/rexmod/script.reds`),
+          destination: path.join(`${REDS_PREFIX}/rexmod/script.reds`),
+        },
+      ],
+    },
+    redsWithMultipleFilesCanonical: {
+      expectedInstallerType: InstallerType.Redscript,
+      inFiles: [
+        ...REDS_PREFIXES,
+        path.join(`${REDS_PREFIX}/rexmod/`),
+        path.join(`${REDS_PREFIX}/rexmod/script.reds`),
+        path.join(`${REDS_PREFIX}/rexmod/notascript.reds`),
+      ],
+      outInstructions: [
+        {
+          type: "copy",
+          source: path.join(`${REDS_PREFIX}/rexmod/script.reds`),
+          destination: path.join(`${REDS_PREFIX}/rexmod/script.reds`),
+        },
+        {
+          type: "copy",
+          source: path.join(`${REDS_PREFIX}/rexmod/notascript.reds`),
+          destination: path.join(`${REDS_PREFIX}/rexmod/notascript.reds`),
+        },
+      ],
+    },
+    redsIncludingNonRedsFilesCanonical: {
+      expectedInstallerType: InstallerType.Redscript,
+      inFiles: [
+        ...REDS_PREFIXES,
+        path.join(`${REDS_PREFIX}/rexmod/`),
+        path.join(`${REDS_PREFIX}/rexmod/script.reds`),
+        path.join(`${REDS_PREFIX}/rexmod/options.json`),
+      ],
+      outInstructions: [
+        {
+          type: "copy",
+          source: path.join(`${REDS_PREFIX}/rexmod/script.reds`),
+          destination: path.join(`${REDS_PREFIX}/rexmod/script.reds`),
+        },
+        {
+          type: "copy",
+          source: path.join(`${REDS_PREFIX}/rexmod/options.json`),
+          destination: path.join(`${REDS_PREFIX}/rexmod/options.json`),
+        },
+      ],
+    },
+    redsSingleScriptTopLevel: {
+      expectedInstallerType: InstallerType.Redscript,
+      inFiles: [path.join(`script.reds`)],
+      outInstructions: [
+        {
+          type: "copy",
+          source: path.join(`script.reds`),
+          destination: path.join(`${REDS_PREFIX}/${FAKE_MOD_NAME}/script.reds`),
+        },
+      ],
+    },
+    redsWithMultipleFilesInRedsBaseDir: {
+      expectedInstallerType: InstallerType.Redscript,
+      inFiles: [
+        ...REDS_PREFIXES,
+        path.join(`${REDS_PREFIX}/`),
+        path.join(`${REDS_PREFIX}/script.reds`),
+        path.join(`${REDS_PREFIX}/notascript.reds`),
+      ],
+      outInstructions: [
+        {
+          type: "copy",
+          source: path.join(`${REDS_PREFIX}/script.reds`),
+          destination: path.join(`${REDS_PREFIX}/${FAKE_MOD_NAME}/script.reds`),
+        },
+        {
+          type: "copy",
+          source: path.join(`${REDS_PREFIX}/notascript.reds`),
+          destination: path.join(`${REDS_PREFIX}/${FAKE_MOD_NAME}/notascript.reds`),
+        },
+      ],
+    },
+  }),
+);
+
+const RedscriptModShouldPromptForInstall = new Map<string, ExamplePromptInstallableMod>(
+  Object.entries({
+    redsWithRedsInToplevelSubdirPromptsOnConflictForFallback: {
+      expectedInstallerType: InstallerType.Redscript,
+      inFiles: [path.join(`rexmod/script.reds`)],
+      proceedLabel: InstallChoices.Proceed,
+      proceedOutInstructions: [
+        {
+          type: "copy",
+          source: path.join(`rexmod/script.reds`),
+          destination: path.join(`rexmod/script.reds`),
+        },
+      ],
+      cancelLabel: InstallChoices.Cancel,
+      cancelErrorMessage: expectedUserCancelMessageFor(InstallerType.Redscript),
+    },
+    redsPatchWithoutCanonical: {
+      expectedInstallerType: InstallerType.Redscript,
+      inFiles: [
+        ...REDS_PREFIXES,
+        path.join(`${REDS_PREFIX}/rexmod/`),
+        path.join(`${REDS_PREFIX}/rexmod/dirname/`),
+        path.join(`${REDS_PREFIX}/rexmod/dirname/patch.reds`),
+      ],
+      proceedLabel: InstallChoices.Proceed,
+      proceedOutInstructions: [
+        {
+          type: "copy",
+          source: path.join(`${REDS_PREFIX}/rexmod/dirname/patch.reds`),
+          destination: path.join(`${REDS_PREFIX}/rexmod/dirname/patch.reds`),
+        },
+      ],
+      cancelLabel: InstallChoices.Cancel,
+      cancelErrorMessage: expectedUserCancelMessageFor(InstallerType.Redscript),
+    },
+  }),
+);
+
+const examples: ExamplesForType = {
+  AllExpectedSuccesses: RedscriptModShouldSucceed,
+  AllExpectedDirectFailures: new Map<string, ExampleFailingMod>(),
+  AllExpectedPromptInstalls: RedscriptModShouldPromptForInstall,
+};
+
+export default examples;
