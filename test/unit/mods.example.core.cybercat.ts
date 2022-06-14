d9c0f6453fc40361ffad96cb1bceeac96f9b565d
Xander Faern <xander@bladehawke.com>
2022-06-14 11:11:15 -0700

    Test includes all dll/json files from the install to ensure not being caught by other installers

--- test/unit/mods.example.core.cybercat.ts
+++ test/unit/mods.example.core.cybercat.ts
@@ -14,6 +14,14 @@ const CoreCyberCatInstall = new Map<string, ExampleSucceedingMod>(
       expectedInstallerType: InstallerType.CoreCyberCat,
       inFiles: [
         path.join("CP2077SaveEditor.exe"),
+        path.join("config.json"),
+        path.join("D3DCompiler_47_cor3.dll"),
+        path.join("e_sqlite3.dll"),
+        path.join("kraken.dll"),
+        path.join("PenImc_cor3.dll"),
+        path.join("PresentationNative_cor3.dll"),
+        path.join("vcruntime140_cor3.dll"),
+        path.join("wpfgfx_cor3.dll"),
         path.join("licenses/"),
         path.join("licenses/CyberCAT.Core.LICENSE.txt"),
         path.join("previews/"),
@@ -25,6 +33,46 @@ const CoreCyberCatInstall = new Map<string, ExampleSucceedingMod>(
           type: "copy",
           source: path.join("CP2077SaveEditor.exe"),
           destination: path.join(CYBERCAT_CORE_BASEDIR, "CP2077SaveEditor.exe"),
+        },
+        {
+          type: "copy",
+          source: path.join("config.json"),
+          destination: path.join(CYBERCAT_CORE_BASEDIR, "config.json"),
+        },
+        {
+          type: "copy",
+          source: path.join("D3DCompiler_47_cor3.dll"),
+          destination: path.join(CYBERCAT_CORE_BASEDIR, "D3DCompiler_47_cor3.dll"),
+        },
+        {
+          type: "copy",
+          source: path.join("e_sqlite3.dll"),
+          destination: path.join(CYBERCAT_CORE_BASEDIR, "e_sqlite3.dll"),
+        },
+        {
+          type: "copy",
+          source: path.join("kraken.dll"),
+          destination: path.join(CYBERCAT_CORE_BASEDIR, "kraken.dll"),
+        },
+        {
+          type: "copy",
+          source: path.join("PenImc_cor3.dll"),
+          destination: path.join(CYBERCAT_CORE_BASEDIR, "PenImc_cor3.dll"),
+        },
+        {
+          type: "copy",
+          source: path.join("PresentationNative_cor3.dll"),
+          destination: path.join(CYBERCAT_CORE_BASEDIR, "PresentationNative_cor3.dll"),
+        },
+        {
+          type: "copy",
+          source: path.join("vcruntime140_cor3.dll"),
+          destination: path.join(CYBERCAT_CORE_BASEDIR, "vcruntime140_cor3.dll"),
+        },
+        {
+          type: "copy",
+          source: path.join("wpfgfx_cor3.dll"),
+          destination: path.join(CYBERCAT_CORE_BASEDIR, "wpfgfx_cor3.dll"),
         },
         {
           type: "copy",
