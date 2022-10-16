e96fd5a44da9529770fd6e2102fc7ca713f58844
Ellie Peterson <ekp2h92@gmail.com>
2022-10-16 15:59:51 -0400

    🚧 migrations?

---
+++ src/migrations.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
+import { VortexMigration } from "./vortex-wrapper";
+
+export const addModTypesMigrationAfter_0_9_0: VortexMigration = async (oldVersion: string): Promise<void> => {
+  Promise.resolve();
+};
