f070ae147a99bd0dc7b653b44787a782e9c86edf
Auska <13802421+effs@users.noreply.github.com>
2022-11-13 08:26:58 +0200

    🏗️ Resolve @vortex-api-test-shimmed as normal external in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,25 @@
-let webpack = require('vortex-api/bin/webpack').default;
+const webpack = require('vortex-api/bin/webpack').default;
 
-module.exports = webpack('cyberpunk2077', __dirname, 4);
+const vortexWebpackConfig =
+ webpack('cyberpunk2077', __dirname, 4);
+
+const finalWebpackConfig = {
+    ...vortexWebpackConfig,
+    externals: {
+        ...vortexWebpackConfig.externals,
+        '@vortex-api-test-shimmed': 'vortex-api',
+    },
+    /*
+    resolve: {
+        ...vortexWebpackConfig.resolve,
+        alias: {
+            ...vortexWebpackConfig.resolve.alias,
+            '@vortex-api-test-shimmed': 'vortex-api',
+        },
+    },
+    */
+};
+
+console.log(vortexWebpackConfig, finalWebpackConfig);
+
+module.exports = finalWebpackConfig;
