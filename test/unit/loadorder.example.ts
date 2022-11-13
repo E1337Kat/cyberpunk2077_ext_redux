bcd1099c35e5140459f10be2c9f706622c2f8f85
Auska <13802421+effs@users.noreply.github.com>
2022-11-13 05:44:13 +0200

    ♻️🧪 Extract and test LO to REDdeploy params conversion
    We need the function to exist independently so that it can be
    invoked from starthooks for our REDdeploy-with-LO Tool.

---
+++ test/unit/loadorder.example.ts
@@ -0,0 +1,147 @@
+import { LoadOrder } from "../../src/load_order.types";
+import { VortexLoadOrderEntry } from "../../src/vortex-wrapper";
+
+
+export const vortexLoadOrder: VortexLoadOrderEntry[] = [{
+  id: `#POPPY DRESS`,
+  enabled: true,
+  name: `✅ #POPPY DRESS (V2077 Autoconverted) 0.0.1-V2077+V2077RED (from #POPPY DRESS.zip )`,
+  data: {
+    ownerVortexProfileId: `JoXXWgtZn`,
+    vortexId: `#POPPY DRESS`,
+    vortexModVersion: ``,
+    vortexEnabled: true,
+    redmodInfo: {
+      name: `#POPPY DRESS (V2077 Autoconverted)`,
+      version: `0.0.1-V2077+V2077RED`,
+      relativePath: `mods\\#POPPY DRESS (V2077 Autoconverted)`,
+      vortexModId: `#POPPY DRESS (V2077)`,
+    },
+  },
+},
+{
+  id: `Guinevere's Always-On Chrome v1.0.0`,
+  enabled: true,
+  name: `✅ AuskaWorks - Guinevere's Always-On Chrome 1.0 (from AuskaWorks Guinevere's Always-On Chrome v1.0.0 - nim's extra more head slots - slot08 enabled )`,
+  data: {
+    ownerVortexProfileId: `JoXXWgtZn`,
+    vortexId: `Guinevere's Always-On Chrome v1.0.0`,
+    vortexModVersion: ``,
+    vortexEnabled: true,
+    redmodInfo: {
+      name: `AuskaWorks - Guinevere's Always-On Chrome`,
+      version: `1.0`,
+      relativePath: `mods\\AuskaWorks - Guinevere's Always-On Chrome`,
+      vortexModId: `Guinevere's Always-On Chrome v1.0.0 (V2077)`,
+    },
+  },
+},
+{
+  id: `redmod version-5401-1-e3-1664115429`,
+  modId: `5401`,
+  enabled: true,
+  name: `✅ Better_Apartment_Views 1.e3 (from Better Apartment Views - redmod version 1.e3)`,
+  data: {
+    ownerVortexProfileId: `JoXXWgtZn`,
+    vortexId: `redmod version-5401-1-e3-1664115429`,
+    vortexModId: `5401`,
+    vortexModVersion: `1.e3`,
+    vortexEnabled: true,
+    redmodInfo: {
+      name: `Better_Apartment_Views`,
+      version: `1.e3`,
+      relativePath: `mods\\Better_Apartment_Views`,
+      vortexModId: `5401`,
+    },
+  },
+},
+{
+  id: `Panam Romanced Enhanced v1.4 REDmod-4626-1-4-1663254950`,
+  modId: `4626`,
+  enabled: true,
+  name: `✅ PanamRomancedEnhanced 1.4 (from Panam Romanced Enhanced v1.4 REDmod 1.4)`,
+  data: {
+    ownerVortexProfileId: `JoXXWgtZn`,
+    vortexId: `Panam Romanced Enhanced v1.4 REDmod-4626-1-4-1663254950`,
+    vortexModId: `4626`,
+    vortexModVersion: `1.4`,
+    vortexEnabled: true,
+    redmodInfo: {
+      name: `PanamRomancedEnhanced`,
+      version: `1.4`,
+      relativePath: `mods\\PanamRomancedEnhanced`,
+      vortexModId: `4626`,
+    },
+  },
+},
+{
+  id: `Panam Romanced Enhanced v1.4 REDmod-4626-1-4-1663254950-V2077-1`,
+  modId: `4626-V2077-1`,
+  enabled: true,
+  name: `✅ PanamRomancedEnhancedPrivacy 1.4 (from Panam Romanced Enhanced v1.4 REDmod 1.4)`,
+  data: {
+    ownerVortexProfileId: `JoXXWgtZn`,
+    vortexId: `Panam Romanced Enhanced v1.4 REDmod-4626-1-4-1663254950`,
+    vortexModId: `4626`,
+    vortexModVersion: `1.4`,
+    vortexEnabled: true,
+    redmodInfo: {
+      name: `PanamRomancedEnhancedPrivacy`,
+      version: `1.4`,
+      relativePath: `mods\\PanamRomancedEnhancedPrivacy`,
+      vortexModId: `4626`,
+    },
+  },
+},
+];
+
+export const v2077LoadOrder: LoadOrder = {
+  loadOrderFormatVersion: `1.0.0`,
+  ownerVortexProfileId: `xyZzyZx`,
+  generatedAt: `2022-11-11T06:43:55.038Z`,
+  entriesInOrderWithEarlierWinning: [
+    {
+      vortexId: `#POPPY DRESS`,
+      vortexModVersion: ``,
+      redmodName: `#POPPY DRESS (V2077 Autoconverted)`,
+      redmodVersion: `0.0.1-V2077+V2077RED`,
+      redmodPath: `mods\\#POPPY DRESS (V2077 Autoconverted)`,
+      enabled: true,
+    },
+    {
+      vortexId: `Guinevere's Always-On Chrome v1.0.0`,
+      vortexModVersion: ``,
+      redmodName: `AuskaWorks - Guinevere's Always-On Chrome`,
+      redmodVersion: `1.0`,
+      redmodPath: `mods\\AuskaWorks - Guinevere's Always-On Chrome`,
+      enabled: true,
+    },
+    {
+      vortexId: `redmod version-5401-1-e3-1664115429`,
+      vortexModId: `5401`,
+      vortexModVersion: `1.e3`,
+      redmodName: `Better_Apartment_Views`,
+      redmodVersion: `1.e3`,
+      redmodPath: `mods\\Better_Apartment_Views`,
+      enabled: true,
+    },
+    {
+      vortexId: `Panam Romanced Enhanced v1.4 REDmod-4626-1-4-1663254950`,
+      vortexModId: `4626`,
+      vortexModVersion: `1.4`,
+      redmodName: `PanamRomancedEnhanced`,
+      redmodVersion: `1.4`,
+      redmodPath: `mods\\PanamRomancedEnhanced`,
+      enabled: true,
+    },
+    {
+      vortexId: `Panam Romanced Enhanced v1.4 REDmod-4626-1-4-1663254950`,
+      vortexModId: `4626`,
+      vortexModVersion: `1.4`,
+      redmodName: `PanamRomancedEnhancedPrivacy`,
+      redmodVersion: `1.4`,
+      redmodPath: `mods\\PanamRomancedEnhancedPrivacy`,
+      enabled: true,
+    },
+  ],
+};
