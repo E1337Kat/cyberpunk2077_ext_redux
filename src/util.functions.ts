f026fdc8a33eb227d049a91883c3bfd4c622b51c
Auska <13802421+effs@users.noreply.github.com>
2022-11-15 09:17:54 +0200

    🏷️ `Effect` type + couple funcs for safe pure side-effects

--- src/util.functions.ts
+++ src/util.functions.ts
@@ -1,9 +1,19 @@
 import {
+  Either,
+  left,
+  right,
+  traverseArray as traverseArrayE,
+} from "fp-ts/lib/Either";
+import {
   flow,
   pipe,
 } from "fp-ts/lib/function";
-import { reduceRight } from "fp-ts/lib/ReadonlyArray";
-import { replace as replaceIn } from "fp-ts/lib/string";
+import {
+  reduceRight,
+} from "fp-ts/lib/ReadonlyArray";
+import {
+  replace as replaceIn,
+} from "fp-ts/lib/string";
 
 //
 // Utility stuff. Judge /very/ carefully before adding anything here.
@@ -24,6 +34,14 @@ export interface Versioned {
 // Functions
 //
 
+// Stringification helpers
+
+export const jsonp = (thing: unknown): string => JSON.stringify(thing);
+export const jsonpp = (thing: unknown): string => JSON.stringify(thing, null, 2);
+
+export const S = (thing: unknown): string =>
+  jsonp(thing)?.replace(/(\t|\n|\r)+/gm, ` `).replace(/\\+"/gm, `"`) ?? `<failed to stringify ${thing}>`;
+
 
 export const identity = <T>(t: T): T => t;
 export const trueish = <T>(t: T): boolean => !!t;
@@ -35,6 +53,26 @@ export const alwaysFalse = (): boolean => false;
 export const constant = <T>(t: T): Dynamic<T> => () => t;
 
 export const noop = (): void => undefined;
+
+export type Effect = () => Either<Error, void>;
+
+export const forEffect = <F extends () => unknown>(f: F): Effect =>
+  () => {
+    try {
+      f();
+    } catch (err) {
+      return left(new Error(`forEffect-wrapped function failed: ${S(err)}`));
+    }
+
+    return right(null);
+  };
+
+export const forEachEffect = (effects: Effect[]): Either<Error, readonly void[]> =>
+  pipe(
+    effects,
+    traverseArrayE((effect) => effect()),
+  );
+
 
 export type GenericRecord =
   | unknown
@@ -51,12 +89,6 @@ export const nestedRecordFrom =
 export const exhaustiveMatchFailure = (_: never): never => {
   throw new Error(`Type guard failed`);
 };
-
-export const jsonp = (thing: unknown): string => JSON.stringify(thing);
-export const jsonpp = (thing: unknown): string => JSON.stringify(thing, null, 2);
-
-export const S = (thing: unknown): string =>
-  jsonp(thing)?.replace(/(\t|\n|\r)+/gm, ` `).replace(/\\+"/gm, `"`) ?? `<failed to stringify ${thing}>`;
 
 
 export const heredoc = flow(
